584281c6a8ba4b450f26cb5a1f33e76d818900ec
withvoidwithin <92968799+withvoidwithin@users.noreply.github.com>
2026-05-29 23:41:45 +0300

    Refactor GameUI initialization functions for improved context handling

--- utils.js
+++ utils.js
@@ -4,16 +4,36 @@
 ////               https://withvoidwithin.github.io/
 //// ════════════════════════════════════════════════════════════════
 
-GameUI._InitContextSnippets = function(context){
-    for(const panel of context.Children()){
-        const snippet = panel.GetAttributeString("snippet", "none")
+GameUI._InitPanel = function(context, panel){
+    for(const child of panel.Children()){
+        const callback_string = child.GetAttributeString("_init", "none")
 
-        if(snippet != "none" && snippet != "__loaded"){
-            panel.SetAttributeString("snippet", "__loaded")
-            panel.RemoveAndDeleteChildren()
-            panel.BLoadLayoutSnippet(snippet)
+        if(callback_string != "none" && callback_string != "__inited"){
+            child.SetAttributeString("_init", "__inited")
+            
+            context.__init_panel = child
+
+            const is_empty = /\(\s*\)$/.test(callback_string)
+            const run_string = callback_string.replace(/\s*\)\s*$/, `${is_empty ? "" : ", "}$.GetContextPanel().__init_panel)`)
+
+            context.RunScriptInPanelContext(run_string)
+            delete context.__init_panel
         }
 
-        GameUI._InitContextSnippets(panel)
+        GameUI._InitPanel(context, child)
+    }
+}
+
+GameUI._InitPanelSnippet = function(panel){
+    for(const child of panel.Children()){
+        const snippet = child.GetAttributeString("_snippet", "none")
+
+        if(snippet != "none" && snippet != "__loaded"){
+            child.SetAttributeString("_snippet", "__loaded")
+            child.RemoveAndDeleteChildren()
+            child.BLoadLayoutSnippet(snippet)
+        }
+
+        GameUI._InitPanelSnippet(child)
     }
 }
